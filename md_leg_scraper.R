fb07454a4037ca0f20907138d2ad8c85bfc7f429
Greg Morton <gregorymorton96@gmail.com>
2023-01-30 00:41:51 -0500

    fix(ed)

--- md_leg_scraper.R
+++ md_leg_scraper.R
@@ -23,7 +23,7 @@ rD <- rsDriver(browser = "firefox",
                chromever = "latest", 
                geckover = "latest", 
                extraCapabilities = list("moz:firefoxOptions" = list(
-                 args = list('--headless'))).
+                 args = list('--headless'))),
                verbose = F
                )
 
